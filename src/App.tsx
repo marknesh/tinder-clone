3aff9dd98bd0e973e4d3bb184793171344543e31
nesh <markmunene.nesh@gmail.com>
2021-12-31 12:55:45 +0300

    add routed to specific paths

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,34 @@ import React from 'react';
 import { FC } from 'react';
 import { Navbar } from './components/Navbar';
 import { SwipeCards } from './components/SwipeCards';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
+import { LikedPics } from './components/LikedPics';
 
 
 
 const App:FC=()=> {
   return (
+    <Router>
     <div className='bg-[whitesmoke] min-h-screen'>
    <Navbar/>
+   <Routes>
 
-   <SwipeCards/>
+<Route path="/" element={<SwipeCards/>} />
+
+<Route path="/likedPics" element={<LikedPics/>} />
+   
+
+
+   </Routes>
+
+   
     </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
 import tinderlogo from "../images/tinderlogo.png"
 
 
 
 export const Navbar:FC = () => {
     return (
-        <div className='flex justify-center py-2'>
-            <img className='w-20 object-contain cursor-pointer' src={tinderlogo} alt="tinder logo" />
+        <div className='flex justify-between w-6/12   p-2'>
+          <Link to="/likedPics"> <button className='text-white py-1 bg-red-500 text-lg cursor-pointer hover:bg-white hover:border-red-500 border-2 hover:text-red-500 rounded-lg px-4'>Liked Pics</button></Link> 
+          <Link to="/"><img className='w-20 object-contain cursor-pointer' src={tinderlogo} alt="tinder logo" /></Link>  
         </div>
     )
 }
